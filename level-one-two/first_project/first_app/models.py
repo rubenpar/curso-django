1179f90165efc5bee0cd5db2106a3f81158ba2bd
Unknown <rubenparnu@gmail.com>
2018-07-12 20:18:05 +0200

    safe commit

--- level-one-two/first_project/first_app/models.py
+++ level-one-two/first_project/first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharFiled(max_lenght=264,unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage (models.Model):
+    
